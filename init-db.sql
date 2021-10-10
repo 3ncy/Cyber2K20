af209b594770ba927e6bfab1490eaee1d2a70fa3
H7KZ <honzik.kominek@gmail.com>
2021-10-10 21:07:07 +0200

    somehow column userReferer is unknown...

--- init-db.sql
+++ init-db.sql
@@ -1,3 +1,17 @@
 CREATE DATABASE IF NOT EXISTS cyber2k20;
-CREATE TABLE IF NOT EXISTS contactForm (email VARCHAR(255), username VARCHAR(255), content TEXT);
-CREATE TABLE IF NOT EXISTS userData (userIP TEXT, userAgent TEXT, userLanguage TEXT, userReferer TEXT, userDate TEXT);
+
+DROP TABLE IF EXISTS contactForm;
+CREATE TABLE IF NOT EXISTS contactForm (
+    email VARCHAR(255), 
+    username VARCHAR(255), 
+    content TEXT
+);
+
+DROP TABLE IF EXISTS userData;
+CREATE TABLE IF NOT EXISTS userData (
+    userIP TEXT, 
+    userAgent TEXT, 
+    userLanguage TEXT, 
+    userReferer TEXT, 
+    userDate TEXT
+);
