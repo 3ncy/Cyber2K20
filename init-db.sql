eebc5dc54566e02a068cf306094ce2a021e3c6b9
H7KZ <honzik.kominek@gmail.com>
2021-10-10 21:24:38 +0200

    uploading to server for test of sql

--- init-db.sql
+++ init-db.sql
@@ -1,8 +1,15 @@
 CREATE DATABASE IF NOT EXISTS cyber2k20;
 
-DROP TABLE IF EXISTS contactForm;
 CREATE TABLE IF NOT EXISTS contactForm (
     email VARCHAR(255), 
     username VARCHAR(255), 
     content TEXT
+);
+
+CREATE TABLE IF NOT EXISTS userData (
+    userIP TEXT, 
+    userAgent TEXT, 
+    userLanguage TEXT, 
+    userReferer TEXT, 
+    userDate TEXT
 );
