9b715315fb910696b88d7b3e39ed3c9eec828621
H7KZ <honzik.kominek@gmail.com>
2021-10-12 22:21:50 +0200

    removed getting users location, because im going to do it through ip address in express

--- public/cookie.js
+++ public/cookie.js
@@ -1,16 +1,3 @@
-const getUserLocation;
-
-if (navigator.geolocation) {
-
-    getUserLocation = navigator.geolocation.getCurrentPosition();
-
-} else {
-    getUserLocation = "couldnt get users location";
-}
-
-fetch('/userdata/location', { method: 'POST', headers: { 'Content-Type': 'application/json'}, 
-body: JSON.stringify({ userLocation: getUserLocation})});
-
 const cookieAccept = document.getElementById("cookie-accept");
 
 cookieAccept.addEventListener("click", (e) => {
