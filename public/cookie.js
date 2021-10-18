5a53218a57565fb9f52472bbe2b1066a47c70029
H7KZ <honzik.kominek@gmail.com>
2021-10-18 12:37:17 +0200

    added html & css countdown - later connecting it so it works

--- public/cookie.js
+++ public/cookie.js
@@ -1,8 +1,39 @@
-const cookieAccept = document.getElementById("cookie-accept");
+/*const cookieAccept = document.getElementById("cookie-accept");
+
+const time = new Date;
+
+let once = false;
+
+var timeLoadedSeconds;
+var timeLoadedMinutes;
+var timeLoaded;
+
+var timeClickedSeconds;
+var timeClickedMinutes;
+var timeClicked;
+
+function timeLoad() {
+    timeLoadedSeconds = time.getSeconds();
+    timeLoadedMinutes = time.getMinutes() * 60;
+    timeLoaded = timeLoadedSeconds + timeLoadedMinutes;
+    once = true;
+}
+
+function timeLoadOnce() {
+    if(!once) timeLoad();
+}
+            
 
 cookieAccept.addEventListener("click", (e) => {
     e.preventDefault();
 
-    document.getElementById("hide-cookie").style.visibility = "hidden";
+    timeClickedSeconds = time.getSeconds();
+    timeClickedMinutes = time.getMinutes() * 60;
+    timeClicked = timeClickedSeconds + timeClickedMinutes;
 
-});
+    var finalTime;
+
+    finalTime = timeClicked - timeLoaded;
+
+    document.getElementById("cookie-text-id").innerHTML = finalTime + 's';
+});*/
